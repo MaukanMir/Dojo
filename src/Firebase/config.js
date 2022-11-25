3b0a5b876bc78b73a356bcc87216b9882ac511d2
Maukan Mir <e-source@E1069.local>
2022-11-25 11:16:10 -0500

    Added firebase modules

---
+++ src/Firebase/config.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD9KDcWqo0UIBKIwxRBgK09FF_CIs5d28U",
+    authDomain: "thedojosite-8a597.firebaseapp.com",
+    projectId: "thedojosite-8a597",
+    storageBucket: "thedojosite-8a597.appspot.com",
+    messagingSenderId: "767586283306",
+    appId: "1:767586283306:web:848595984305a04f1bb853"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+// init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+//time stamp
+const timestamp = firebase.firestore.Timestamp;
+
+export {projectAuth, projectFirestore, timestamp};
